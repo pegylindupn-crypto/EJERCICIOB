68bcd78b74596c9d1ef2bbeb998c4f3805eafcc5
Linda <pegylind.upn@gmail.com>
2025-09-28 12:50:37 -0500

    practicampo4

---
+++ PractiCampo4/PracCampo_Caso1/PracCampo_Caso1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracCampo_Caso1
+{
+    internal class PracCampo4_Caso1
+    {
+
+        public static double CelsiusFahrenheit()
+        {
+            double C, F;//declaración de variables (C)Celsius, (F)Fahrenheit
+
+            Console.WriteLine("CONVERTIR GRADOS CELSIUS A GRADOS FAHRENHEIT");
+            Console.WriteLine("--------------------------------------------");
+
+            //solicitar al usuario ingresar la temperatura a grados celsius
+            Console.WriteLine("Ingrese la temperatura en grados Celsius: ");
+
+            //lee la entrada del usuario y convertir a double
+            if (!double.TryParse(Console.ReadLine(), out C) || C < -273.15)
+            {
+                // -273.15 valor mínimo de la temperatura celsius, cero absoluto
+
+                Console.WriteLine("Temperatura no válida.Se considerará como 0°");
+                C = 0;
+            }
+            //Formula
+            F = (C * 9 / 5) + 32;
+
+            return F;
+
+        }
+        static void Main(string[] args)
+        {
+            //LLamada al método o función 
+            double F = CelsiusFahrenheit();
+
+            //Muestra resultado
+            Console.WriteLine($"La temperatura en Fahrenheit es: {F}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PractiCampo4/PracCampo_Caso1/PracCampo_Caso2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracCampo_Caso2
+{
+    internal class PracCampo4_Caso2
+    {
+        //Funcion para verificar número primo,
+        public static bool VerifNumPrimo(bool NumPrimo = true)
+        {
+            Console.WriteLine("VERIFICAR SI UN NUMERO ES PRIMO");
+            Console.WriteLine("-------------------------------");
+
+            //solicitar al usuario ingresar un número
+            Console.WriteLine("Ingresar un número:");
+
+            //Lee y valida que el numero sea entero y menor,igual a 1
+            if (!int.TryParse(Console.ReadLine(), out int num) || num <= 1)
+            {
+                NumPrimo = false;
+            }
+            else
+            {   //con 'Math.Sqrt' calcula la raíz cuadrada para mejorar el bucle de verificación
+                for (int i = 2; i <= Math.Sqrt(num); i++) //si el número es divisible por "i" no es primo
+                {
+                    if (num % i == 0)
+                    {
+                        NumPrimo = false;
+                        break;
+                    }
+                }
+            }
+            return NumPrimo; //devuelve el resultado
+        }
+
+        static void Main(string[] args)
+        {
+            bool resultado = VerifNumPrimo();
+
+            //estructura condicional para mostral el mensaje final
+            if (resultado)
+            {
+                Console.WriteLine("El número es primo");
+
+            }
+            else
+            {
+                Console.WriteLine($"El número no es primo");
+            }
+            Console.ReadKey();
+        }
+    }
+}
